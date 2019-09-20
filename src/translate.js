44fa4e3b184d0e06b6bfb6612142e5bb8e55871e
prav <kotecek3@gmail.com>
2019-09-20 16:26:39 +0200

    fix for electron

--- src/translate.js
+++ src/translate.js
@@ -1,28 +1,10 @@
-const https = require('https');
+const request = require('request-promise-native');
 module.exports.AVAILABLE_LANGUAGES = ['af', 'sq', 'ar', 'az', 'eu', 'bn', 'be', 'bg', 'ca', 'zh-CN', 'zh-TW', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'iw', 'hi', 'hu', 'is', 'id', 'ga', 'it', 'ja', 'kn', 'ko', 'la', 'lv', 'lt', 'mk', 'ms', 'mt', 'no', 'fa', 'pl', 'pt', 'ro', 'ru', 'sr', 'sk', 'sl', 'es', 'sw', 'sv', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'vi', 'cy', 'yi', 'any'];
-
-async function get(url) {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, response => {
-        let body = '';
-        response.on('data', d => body += d);
-        response.on('end', () => {
-          try {
-            resolve(JSON.parse(body));
-          } catch (e) {
-            reject(e);
-          }
-        });
-      })
-      .on('error', reject);
-  });
-}
 
 module.exports.translate = async function translate(text, translateTo, translateFrom = 'auto') {
   const url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl='
     + translateFrom + '&tl=' + translateTo + '&dt=t&q=' + encodeURI(text);
 
-  const body = await get(url);
+  const body = await request.get(url, {json:true});
   return body[0][0][0];
 };
